7f7c8f4f08035d9c63781eda9c8b74f6fac4f22e
myk4040okothogodo <mikeogodo5@gmail.com>
2022-01-11 13:41:56 +0300

    additional features

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,21 +3,25 @@ import { StyledMenu } from './Menu.styled';
 import { bool } from 'prop-types';
 
 
-const Menu = ({ open }) => {
+const Menu = ({ open, ...props }) => {
+
+  const isHidden = open ? true : false;
+  const tabIndex = isHidden ? 0 : -1;
+  
   return (
-    <StyledMenu open={open}>
-     <a href ="/">
-       <span role="img" aria-label="about us">&#x1f481;&#x1f3fb;&#x200d;&#x2642;&#xfe0f;</span>
-       About us
-     </a>
-      <a href="/">
-      <span role="img" aria-label="price">&#x1f4b8;</span>
-      Pricing
+    <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
+      <a href="/" tabIndex={tabIndex}>
+        <span aria-hidden="true">💁🏻‍♂️</span>
+        About us
       </a>
-     <a href="/">
-      <span role="img" aria-label="contact">&#x1f4e9;</span>
-      Contact
-       </a>
+      <a href="/" tabIndex={tabIndex}>
+        <span aria-hidden="true">💸</span>
+        Pricing
+        </a>
+      <a href="/" tabIndex={tabIndex}>
+        <span aria-hidden="true">📩</span>
+        Contact
+        </a>
     </StyledMenu>
   )
 }
